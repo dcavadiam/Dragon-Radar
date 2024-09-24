2cc1af9d4e0d83560db5ee35bb94a05f513e62c7
dcavadia <dcavadia@uninorte.edu.co>
2024-09-24 14:19:41 -0500

    add chevrons buttons

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { useQuery } from 'react-query';
 import { fetchData } from "@/services/fetchApi";
-import { ChevronLeft, ChevronRight } from "@/lib/icons/arrows";
+import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight } from "@/lib/icons/arrows";
 import { useState } from "react";
 
 export default function Home() {
@@ -50,7 +50,9 @@ export default function Home() {
         </ul>
       </div>
       <div className="flex justify-center items-center w-full gap-3">
-       
+        <span className="px-4 py-2 bg-gray-800 rounded-lg">
+          <ChevronsLeft />
+        </span>
         <span className="px-4 py-2 bg-gray-800 rounded-lg cursor-pointer" onClick={handlePreviousPage}>
           <ChevronLeft />
         </span>
@@ -58,7 +60,9 @@ export default function Home() {
         <span className="px-4 py-2 bg-gray-800 rounded-lg cursor-pointer" onClick={handleNextPage}>
           <ChevronRight />
         </span>
-        
+        <span className="px-4 py-2 bg-gray-800 rounded-lg ">
+          <ChevronsRight />
+        </span>
       </div>
       <footer className="flex flex-col items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
         <p className="text-center text-gray-400">
@@ -68,3 +72,4 @@ export default function Home() {
     </div>
   )
 }
+

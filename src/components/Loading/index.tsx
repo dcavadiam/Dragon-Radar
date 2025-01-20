406d4acf9e1a1c5b4a69a21f0f347adad5b97da4
Diego <dcavadia@uninorte.edu.co>
2025-01-20 14:08:31 -0500

    🔧fix: image optimized

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const Loading = () => {
     return <main className="flex flex-col justify-center items-center min-h-screen">
-        <Image src="/dragon-ball-with-4-stars.png" alt="dragon ball with 4 stars" width={50} height={50} className="aspect-auto animate-spin" />
+        <Image src="/dragon-ball-with-4-stars.webp" alt="dragon ball with 4 stars" width={50} height={50} className="aspect-auto animate-spin" />
         <h2>Cargando...</h2>
     </main>;
 }
